15c8d1f3d6581f40b2cea71a0ce1bf88c51d612b
Bryan Johns <bjohns@greendragonweb.com>
2013-01-28 18:40:30 -0600

    More experiments with globalization.

--- K4GDW.Url.Shortener.WPF/Application.xaml.vb
+++ K4GDW.Url.Shortener.WPF/Application.xaml.vb
@@ -1,6 +1,14 @@
-﻿Class Application
+﻿Imports System.Globalization
+Imports System.Windows.Markup
 
-    ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
-    ' can be handled in this file.
 
+Class Application
+	' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
+	' can be handled in this file.
+
+	Private Sub Application_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
+		FrameworkElement.LanguageProperty.OverrideMetadata(GetType(FrameworkElement),
+		                                                   New FrameworkPropertyMetadata(
+			                                                   XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)))
+	End Sub
 End Class
