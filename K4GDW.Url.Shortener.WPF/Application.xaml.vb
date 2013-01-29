5b7acf2221373251cbbbbee5172c8ae027433abc
Bryan Johns <bryan.johns@mh.alabama.gov>
2013-01-29 07:36:55 -0600

    Removed the spanish resource file until I can learn more about actually using it.  Even though I've followed all of the instructions I've found so far, the app seems to ignore the translations when I change the PC's default culture to Spanish.  Not sure why.  This feature will wait until I actually get demand for it and/or I figure out how to implement it.
    modified:   Build/build.msbuild
    modified:   K4GDW.Url.Shortener.WPF/Application.xaml.vb
    modified:   K4GDW.Url.Shortener.WPF/K4GDW.Url.Shortener.WPF.vbproj
    deleted:    K4GDW.Url.Shortener.WPF/Resources.ES.Designer.vb
    deleted:    K4GDW.Url.Shortener.WPF/Resources.ES.resx

--- K4GDW.Url.Shortener.WPF/Application.xaml.vb
+++ K4GDW.Url.Shortener.WPF/Application.xaml.vb
@@ -6,9 +6,4 @@ Class Application
 	' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
 	' can be handled in this file.
 
-	Private Sub Application_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
-		FrameworkElement.LanguageProperty.OverrideMetadata(GetType(FrameworkElement),
-		                                                   New FrameworkPropertyMetadata(
-			                                                   XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)))
-	End Sub
 End Class
