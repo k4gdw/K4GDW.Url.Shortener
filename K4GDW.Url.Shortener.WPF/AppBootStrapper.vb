314ddbad2f79d0f0660117b59b44123fc1baf894
unknown <bryan.johns@mh.alabama.gov>
2013-05-13 09:38:30 -0500

    Renamed the local privatel variable "container" to "_container" to get it in compliance with standard naming conventions.

--- K4GDW.Url.Shortener.WPF/AppBootStrapper.vb
+++ K4GDW.Url.Shortener.WPF/AppBootStrapper.vb
@@ -14,47 +14,47 @@ Imports K4GDW.Infrastructure.Logging
 Public Class AppBootStrapper
 	Inherits Bootstrapper(Of MainViewModel)
 
-	Private container As CompositionContainer
+    Private _container As CompositionContainer
 
-	Protected Overrides Sub Configure()
-		container = New CompositionContainer(
-						New AggregateCatalog(
-							AssemblySource.Instance.Select(Function(x) New AssemblyCatalog(x)) _
-							.OfType(Of ComposablePartCatalog)()))
-		Dim batch As New CompositionBatch
-		batch.AddExportedValue(Of IWindowManager)(New WindowManager)
-		batch.AddExportedValue(Of IEventAggregator)(New EventAggregator)
-		batch.AddExportedValue(Of ILogger)(New NLogger)
-		batch.AddExportedValue(Of AppConfiguration)(Config())
-		container.Compose(batch)
-	End Sub
+    Protected Overrides Sub Configure()
+        _container = New CompositionContainer(
+                        New AggregateCatalog(
+                            AssemblySource.Instance.Select(Function(x) New AssemblyCatalog(x)) _
+                            .OfType(Of ComposablePartCatalog)()))
+        Dim batch As New CompositionBatch
+        batch.AddExportedValue(Of IWindowManager)(New WindowManager)
+        batch.AddExportedValue(Of IEventAggregator)(New EventAggregator)
+        batch.AddExportedValue(Of ILogger)(New NLogger)
+        batch.AddExportedValue(Of AppConfiguration)(Config())
+        _container.Compose(batch)
+    End Sub
 
-	Private Function Config() As AppConfiguration
-		Dim configFolder As String = String.Format("{0}/K4GDW Software/Url.Shortener", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData))
-		If Not Directory.Exists(configFolder) Then
-			Directory.CreateDirectory(configFolder)
-		End If
-		Dim configProvider As New ConfigurationFileConfigurationProvider(Of AppConfiguration) With {
-				.ConfigurationSection = "Settings",
-				.ConfigurationFile = String.Format("{0}\UrlShortenerConfig.xml", configFolder),
-				.PropertiesToEncrypt = "BitLyKey,BitLyLogin",
-				.EncryptionKey = "8cA_5yT&"}
-		Dim cnf As New AppConfiguration
-		cnf.Initialize(configProvider)
-		Return cnf
-	End Function
+    Private Function Config() As AppConfiguration
+        Dim configFolder As String = String.Format("{0}/K4GDW Software/Url.Shortener", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData))
+        If Not Directory.Exists(configFolder) Then
+            Directory.CreateDirectory(configFolder)
+        End If
+        Dim configProvider As New ConfigurationFileConfigurationProvider(Of AppConfiguration) With {
+                .ConfigurationSection = "Settings",
+                .ConfigurationFile = String.Format("{0}\UrlShortenerConfig.xml", configFolder),
+                .PropertiesToEncrypt = "BitLyKey,BitLyLogin",
+                .EncryptionKey = "8cA_5yT&"}
+        Dim cnf As New AppConfiguration
+        cnf.Initialize(configProvider)
+        Return cnf
+    End Function
 
-	Protected Overrides Function GetInstance(serviceType As Type,
-											 key As String) As Object
-		Dim contract As String = If(String.IsNullOrEmpty(key),
-									AttributedModelServices.GetContractName(serviceType),
-									key)
-		Dim exports As IEnumerable(Of Object) = container.GetExportedValues(Of Object)(contract)
+    Protected Overrides Function GetInstance(serviceType As Type,
+                                             key As String) As Object
+        Dim contract As String = If(String.IsNullOrEmpty(key),
+                                    AttributedModelServices.GetContractName(serviceType),
+                                    key)
+        Dim exports As IEnumerable(Of Object) = _container.GetExportedValues(Of Object)(contract)
 
-		If exports.Count > 0 Then
-			Return exports.First
-		End If
-		Throw New Exception(String.Format("Could not located any instances of contract {0}.",
-										  contract))
-	End Function
+        If exports.Count > 0 Then
+            Return exports.First
+        End If
+        Throw New Exception(String.Format("Could not located any instances of contract {0}.",
+                                          contract))
+    End Function
 End Class
